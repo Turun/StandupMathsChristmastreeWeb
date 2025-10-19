20fd122dc18de8c83d673ce7acfc359963de354f
Turun <github@turun.de>
2025-10-19 14:53:00 +0200

    reorganizing

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.run(host="0.0.0.0", port="8080")
 
 
 @app.route("/")
 def html():
-    return render_template("../www/index.html")
+    return render_template("index.html")
