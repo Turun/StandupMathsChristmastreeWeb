4d4f9272d950c0258bf6d4acd7b3e8c0d559f07f
Turun <github@turun.de>
2025-10-19 16:01:22 +0200

    JS Calc image differences demo

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ The software architecture consists of several parts:
 There are also python bindings for rust available for the rpi_ws281x library, so we could also use rust to write the webserver. This would make it easier to write multi threaded code, if we need to do such a thing.
 
 One significant challenge will be detecting the position of the LEDs
+
+
+ADDENDUM: It might be hard to figure out where exactly each LED is located, but that's not the thing we are interested in, is it? If the LED has a strong reflection (diffuse, e.g. because of a nearby wall) it is fair to say that the location should be considered the wall and not the actual position of the LED.
+This can be expanded: We don't want to know the position of each led, we want to know how each LED influences each voxel of space. A diffuse influence is expected and can be used for even greater works!!!
--- static/script.js
+++ static/script.js
@@ -1,10 +1,12 @@
 
 // Select HTML elements
 const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const captureButton = document.getElementById('capture-btn');
+const canvasCap1 = document.getElementById('canvas-cap1');
+const canvasCap2 = document.getElementById('canvas-cap2');
+const canvasDiff = document.getElementById('canvas-diff');
+const captureButton1 = document.getElementById('capture-btn1');
+const captureButton2 = document.getElementById('capture-btn2');
 const startButton = document.getElementById('start-btn');
-const context = canvas.getContext('2d');
 
 // Function to start the camera
 function startCamera() {
@@ -19,13 +21,49 @@ function startCamera() {
 }
 
 // Function to capture a photo
-function capturePhoto() {
+function capturePhoto1() {
     // Set canvas dimensions to match video
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    canvasCap1.width = video.videoWidth;
+    canvasCap1.height = video.videoHeight;
     
     // Draw the current video frame to the canvas
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    canvasCap1.getContext("2d").drawImage(video, 0, 0, canvasCap1.width, canvasCap1.height);
+
+    showDifference();
+}
+
+function capturePhoto2() {
+    // Set canvas dimensions to match video
+    canvasCap2.width = video.videoWidth;
+    canvasCap2.height = video.videoHeight;
+    
+    // Draw the current video frame to the canvas
+    canvasCap2.getContext("2d").drawImage(video, 0, 0, canvasCap2.width, canvasCap2.height);
+
+    canvasDiff.width = video.videoWidth;
+    canvasDiff.height = video.videoHeight;
+    canvasDiff.getContext("2d").drawImage(video, 0, 0, canvasDiff.width, canvasDiff.height);
+
+    showDifference();
+}
+
+function showDifference() {
+    const img1 = canvasCap1.getContext("2d").getImageData(0, 0, canvasCap1.width, canvasCap2.height);
+    const img2 = canvasCap2.getContext("2d").getImageData(0, 0, canvasCap2.width, canvasCap2.height);
+    
+    const diff = canvasDiff.getContext("2d").createImageData(img1.width, img1.height);
+    for (let i = 0; i < img1.data.length; i += 4) {
+      const r = Math.abs(img1.data[i] - img2.data[i]);
+      const g = Math.abs(img1.data[i + 1] - img2.data[i + 1]);
+      const b = Math.abs(img1.data[i + 2] - img2.data[i + 2]);
+      // You can adjust contrast/sensitivity here
+      const val = (r + g + b) / 3;
+      diff.data[i] = val;
+      diff.data[i + 1] = val;
+      diff.data[i + 2] = val;
+      diff.data[i + 3] = 255; // opaque
+    }
+    canvasDiff.getContext("2d").putImageData(diff, 0, 0);
 }
 
 function startBackgroundThread() {
@@ -43,7 +81,8 @@ function gotMessage(e) {
 window.addEventListener('load', startCamera);
 
 // Capture photo when button is clicked
-captureButton.addEventListener('click', capturePhoto);
+captureButton1.addEventListener('click', capturePhoto1);
+captureButton2.addEventListener('click', capturePhoto2);
 startButton.addEventListener('click', startBackgroundThread);
 
 const eventSource = new EventSource("/events");
